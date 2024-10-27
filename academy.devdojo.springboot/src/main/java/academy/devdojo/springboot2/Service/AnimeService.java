a37eecfe690abb4176c55915b51d7a41339408c4
Hugo Mendes <hugo.mendes.313@gmail.com>
2024-10-26 22:10:52 -0300

    Solucionado problema da inicialização do mapeamento das dependencias

--- academy.devdojo.springboot/src/main/java/academy/devdojo/springboot2/Service/AnimeService.java
+++ academy.devdojo.springboot/src/main/java/academy/devdojo/springboot2/Service/AnimeService.java
@@ -12,7 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-@Service
+@Service("AnimeRepository")
 @RequiredArgsConstructor
 public class AnimeService {
 
